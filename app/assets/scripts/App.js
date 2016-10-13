50668c40ed1a6663dc7d35aba534fd2a38aec034
Paul74 <superpaolo74@gmail.com>
2016-10-13 17:35:46 +0200

    finita lezione 41

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,19 +1,3 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes(){
-		console.log(this.name + " now pay tax ");
-	}
-}
-
-alert("abc 123");
-
-var john = new Person("john doe","blue");
-john.greet();
-
-var jane = new Adult("jane smith","orange");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
